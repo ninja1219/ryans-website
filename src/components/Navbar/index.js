ebb39b1f426121012b0ec14356cac4e9c1d0776d
Ryan Meyer <rmmeyer19@gmail.com>
2024-05-28 14:25:57 -0600

    Set up basic multipage react app

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+ 
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/" activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to="/tictactoe" activeStyle>
+                        TicTacToe
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
+    );
+};
+ 
+export default Navbar;
