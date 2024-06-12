ad968baa4429e893a394549d25c883c080afff47
Ryan Meyer <rmmeyer19@gmail.com>
2024-06-12 16:27:51 -0600

    Update home and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,17 @@ const About = () => {
     return (
         <div>
             <h1>About Ryan</h1>
+            <p>
+                Ryan graduated from BYU in April 2024 as Cum Laude in Computer Science: Software Engineering Emphasis 
+                with a Mathematics Minor. He hopes to be a software engineer and wants to someday work for a Big Tech 
+                company. He specifically loves coding and thinking through complex algorithms, being involved in the 
+                entire live cycle of a project to see his work in action, and working with REST APIs and large amounts 
+                of data.
+            </p>
+            <p>
+                Outside of programming, Ryan enjoys playing a variety of sports, running half marathons, playing games 
+                and watching movies with friends, wood carving, hiking, and learning new things.
+            </p>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,15 @@ const Home = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <p>This website is for introducing Ryan Meyer, showcasing his experience and 
-                skills, and building fun games and software engineering projects.</p>
+            <p>
+                This website is for introducing Ryan Meyer, showcasing his experience and 
+                skills, and building fun games and software engineering projects.
+            </p>
+            
+            <div style={{"display": "flex", "flex-direction": "column"}}>
+                <a href="https://www.linkedin.com/in/ryan-meyer-74a165244/">LinkedIn Profile</a>
+                <a href="https://github.com/ninja1219">GitHub Profile</a>
+            </div>
         </div>
     );
 };
